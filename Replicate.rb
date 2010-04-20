3683ce95eda004190ebd59f955c84a6bd734767c
theOtherRob <rob@rob-laptop.(none)>
2010-04-19 21:23:18 -0400

    first commit

---
+++ Replicate.rb
@@ -0,0 +1,45 @@
+require 'ReprapDriver'
+
+def prepare(driver)
+driver.print("
+G90;Absolute
+M104 0;Reset temperature
+G28;Home
+G1 F3000
+G1 X50 Y5 F3000
+")
+end
+
+skeinforge_dir = "/usr/local/skeinforge18/skeinforge/"
+port = ReprapDriver.all[0]
+puts port
+driver = ReprapDriver.new(port, true)
+
+lastArg = nil
+ARGV.each do |arg|
+  if lastArg!=nil then
+    if lastArg=="-file" then
+      gcode_file = arg[0..arg.length-5]+"_export.gcode"
+      if arg[arg.length-4..arg.length-1] == ".stl" then
+        # Reset the Printer
+        prepare(driver)
+        # Skeinforge the STL
+        puts "skeinforging.. Please wait an EXTREMELY long time"
+        bb = IO.popen("cd "+skeinforge_dir+";./skeinforge.py "+arg)
+        b = bb.readlines
+        puts b.join
+        # Print the GCode
+        driver.print_file(gcode_file)
+        prepare(driver)
+      elsif arg[arg.length-6..arg.length-1] == ".gcode" then
+        driver.print_file(arg)
+        prepare(driver)
+      end
+    end
+    lastArg = nil
+  elsif arg=="-reset" then
+    prepare(driver)
+  else
+    lastArg = arg
+  end
+end
---
+++ ReprapDriver.rb
@@ -0,0 +1,95 @@
+require 'rubygems'
+require 'serialport'
+#gem install serialport
+
+class ReprapDriver
+  
+  def self.all
+    #POSIX
+    return Dir.glob(["/dev/ttyUSB*"])
+    #Windows
+    #TODO: if windows return other shiz
+  end
+
+  def initialize(portName, checksums)
+    @checksums = checksums
+    @line_counter = -1
+    @port = SerialPort.new portName, 19200, 8, 1, SerialPort::NONE
+    @port.read_timeout=0 #does not return from read until a byte is received
+    reset
+  end
+
+  def reset
+    # Briefly pulse the RTS line low.  On most arduino-based boards, this will hard reset the
+    # device.
+    @port.dtr = 0
+    @port.rts = 0
+    sleep(0.2);
+    @port.dtr = 1
+    @port.rts = 1
+    sleep(0.3); #give the arduino some startup time
+    print("M110") #reset the current line number
+  end
+
+  def print(gcode)
+    gcode.each_line { |line| print_gcode_line(line) }
+  end
+  
+  def print_file(filename)
+    File.open(filename, "r") do |infile|
+      while (line = infile.gets)
+        print_gcode_line(line)
+      end
+    end
+  end
+
+  def print_gcode_line(line)
+    line = line.strip
+    #skip comment and null lines
+    if !(line.length == 0 || line[0] == ';' || line[0] == '(' || line[0] == '\n' || line[0] == '\r') then
+      if line.scan(/[Mm]110/)[0]!=nil then #M110 = reset current line number
+        @line_counter = -1
+      end
+      print_gcode_line!(@line_counter+=1, line)
+    end
+  end
+
+  def print_gcode_line!(line_number, line)
+    # send the line of gcode + any checksum/line numbering and then check/resend bad checksum lines
+    # RepRap Syntax: N<linenumber> <cmd> *<chksum>\n
+    # Makerbot Syntax: <cmd>\n
+    # Comments: ;<comment> OR (<comment>)
+    gcode = line.gsub(/;.*|\(.*?\)/,'').strip
+    while true
+      #remove comments and whitespace from command lines and adding line numbers
+      line = ("N"+line_number.to_s+' '+gcode).strip+' '
+      checksum = 0
+      line.each_byte { |b| checksum ^= b } #calculate the checksum byte
+      line = line +'*'+checksum.to_s+"\n" #add checksum
+	    @port.write line #send the gcode
+      puts line
+      response = ""
+      resend_line = {}
+      while resend_line.length == 0
+        response += @port.getc.chr
+        if response.include?("ok") then
+          if (debug = response.scan(/DEBUG:.*/)[0])!=nil then
+            puts debug #echoing debug infor from serial port
+          end
+          return true # success ^_^
+        end
+        resend_line = response.scan(/Resend:([0-9]*)[^0-9]/)
+      end
+      #== Bad Checksum ==
+      #preparing to resend the requested bad checksum line
+      #(since we wait for the 'ok' response it must be the line we just sent so any other # = fubar)
+      puts response.to_s + "\n<RESPONSE> Re-sending gcode: "+gcode.to_s
+      if line_number != resend_line[0][0] then
+        puts "<RESPONSE> line number is fubared, resetting"
+        print("M110") #reset line number
+        line_number = (@line_counter+=1) #reset the line number for this gcode we've got to resend
+      end
+    end
+  end
+end
+
